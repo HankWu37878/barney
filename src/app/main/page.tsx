c299782dce0d78d9408d90edbeca97354f2e36c0
xrueiii <118975947+xrueiii@users.noreply.github.com>
2024-12-11 08:13:26 +0800

    Update page.tsx

--- src/app/main/page.tsx
+++ src/app/main/page.tsx
@@ -1,5 +1,9 @@
 import { db } from "@/db";
-import { memberAccountTable, orderTable } from "@/db/schema";
+import {
+  customizedOrderTable,
+  memberAccountTable,
+  orderTable,
+} from "@/db/schema";
 import { auth } from "@/lib/auth";
 import { Order } from "@/lib/types";
 import { eq } from "drizzle-orm";
@@ -25,13 +29,44 @@ export default async function Main() {
     )
     .orderBy(orderTable.time)
     .execute();
+
+  const customOrders: Order[] = await db
+    .select({
+      type: customizedOrderTable.type,
+      lname: memberAccountTable.lname,
+      fname: memberAccountTable.fname,
+      time: customizedOrderTable.time,
+      phone: memberAccountTable.memberPhone,
+      orderId: customizedOrderTable.customizedOrderId,
+    })
+    .from(customizedOrderTable)
+    .where(eq(customizedOrderTable.branchId, branchId ?? ""))
+    .leftJoin(
+      memberAccountTable,
+      eq(customizedOrderTable.memberId, memberAccountTable.memberId)
+    )
+    .orderBy(customizedOrderTable.time)
+    .execute();
   const today = new Date();
 
   return (
     <div className="p-6 bg-gray-100 min-h-screen">
       <OrderTable
+        isCustom={false}
         orders={orders.filter(
-          (order) => order.time.toDateString() === today.toDateString()
+          (o) =>
+            o.time.getUTCDate() === today.getDate() &&
+            o.time.getUTCMonth() === today.getMonth() &&
+            o.time.getUTCFullYear() === today.getFullYear()
+        )}
+      />
+      <OrderTable
+        isCustom={true}
+        orders={customOrders.filter(
+          (o) =>
+            o.time.getUTCDate() === today.getDate() &&
+            o.time.getUTCMonth() === today.getMonth() &&
+            o.time.getUTCFullYear() === today.getFullYear()
         )}
       />
     </div>
